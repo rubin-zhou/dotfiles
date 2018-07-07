1a2247346a5b97d81d837be927623149177ed147
lubing <lubing@nutonomy.com>
2018-07-07 10:41:27 +0800

    add vim update, plugins install

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,11 +1,19 @@
-#!/bash/bin
+#!/bin/bash
 
-# copy .vimrc to ~/
+# update vim to latest
+git clone https://github.com/vim/vim.git
 
+
+# YouCompleteme plugin requires Vim compliled with Python 2.x support, build vim from source and install
+cd vim/src && make distclean && cd ../
+cd ..
+./configure --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu
+cd src && make && sudo make install
+
+
+# copy vimrc and install vundle plugins
+cp .vimrc ~/
 mkdir -p ~/.vim/bundle
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle.vim
-
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source ~/.bashrc
-
 vim +PluginInstall +qall
